7f6c1665e6f7e7fedefc37e4343e5536110777fd
Bakhtiyar Tokpayev <btokpayev@gmail.com>
2024-10-29 20:56:37 +0500

    Homework 5

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChatAppDB;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Server server = new Server();
+            server.Operate();
+            //Console.WriteLine("Exiting the server");
+        }
+        else if (args.Length == 1)
+        {
+            Client client = new Client(args[0]);
+            //Console.WriteLine("Exiting the client");
+        }
+    }
+}
